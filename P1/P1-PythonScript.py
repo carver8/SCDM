d1309ab82272bff0752b996a7f974182f71a31ad
Cameron Carver <cameroncarver8@gmail.com>
2024-03-21 12:16:20 +0200

    Update Python script

--- P1/P1-PythonScript.py
+++ P1/P1-PythonScript.py
@@ -1,3 +1,3 @@
 import pandas as pd
-ctd081129 = pd.read_csv('/Users/crcarver/Documents/GitHub/P1/CTD-20081129-DTS.dat', sep='\t')
+ctd081129 = pd.read_csv('/Users/crcarver/Documents/Repository/SCDM/P1/CTD-20081129-0652-DTS.dat', sep='\t')
 print(ctd081129)
